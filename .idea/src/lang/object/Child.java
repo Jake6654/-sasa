72780a9cdc7715abbb9cdeaf82ea110dc281a930
Jake <ugw2ad@virginia.edu>
2024-09-07 12:34:26 -0400

    INITIALIZING

--- .idea/src/lang/object/Child.java
+++
@@ -1,9 +0,0 @@
-package lang.object;
-
-public class Child extends Parent{
-    // 명시적으로 상속을 받았기 때문에 Object 클래스가 자동으로 상속받지 않는다.
-
-    public void childMethod(){
-        System.out.println("Child.childMethod");
-    }
-}
--- .idea/src/lang/string/immutable/StringImmutable1.java
+++
@@ -1,9 +0,0 @@
-package lang.string.immutable;
-
-public class StringImmutable1 {
-    public static void main(String[] args) {
-        String str = "hello";
-        String newStr = str.concat(" java");
-        System.out.println("str = " + newStr);
-    }
-}
